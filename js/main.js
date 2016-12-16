c667a577612236715f62475b58939274734e03b6
levg34 <guilhem.dross@laposte.net>
2016-12-16 01:17:58 +0100

    working algorithm, limited board

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
+BOARD = {
+	x:7,
+	y:7
+}
+
 function equals(a, b) {
-	JSON.stringify(a) === JSON.stringify(b)
+	return JSON.stringify(a) === JSON.stringify(b)
 }
 
 // define classes
+
+// Move
+function Move() {
+	//
+}
+
+Move.prototype.moves = [{x:1,y:2},{x:2,y:1},{x:2,y:-1},{x:1,y:-2},{x:-1,y:-2},{x:-2,y:-1},{x:-2,y:1},{x:-1,y:2}]
+
+Move.prototype.randomMove = function() {
+	var m = Math.floor(Math.random() * 8)
+	return this.moves[m]
+}
 
 // Point
 function Point(x,y) {
@@ -10,21 +27,25 @@ function Point(x,y) {
 	this.y=y
 }
 
+Point.prototype.inBoard = function() {
+	return this.x >= 0 && this.y >= 0 && this.x <= BOARD.x && this.y <= BOARD.y
+}
+
 Point.prototype.move = function(m) {
 	var res = new Point()
 
 	res.x = this.x+m.x
 	res.y = this.y+m.y
+	
+	if (!res.inBoard()) {
+		res = this
+	}
 
 	return res
 }
 
-// Move
-Move = [{x:1,y:2},{x:2,y:1},{x:2,y:-1},{x:1,y:-2},{x:-1,y:-2},{x:-2,y:-1},{x:-2,y:1},{x:-1,y:2}]
-
-function randomMove() {
-	var m = Math.floor(Math.random() * 8)
-	return Move[m]
+Point.prototype.distance = function(b) {
+	return Math.sqrt(Math.pow(b.x-this.x,2)+Math.pow(b.y-this.y,2))
 }
 
 // Route (solution)
@@ -50,10 +71,11 @@ Problem.prototype.solve = function() {
 	var C = this.start_pt
 
 	do {
-		var rmove = randomMove()
-		route.addMove(rmove)
-		C = C.move(rmove)
-		console.log(C)
+		var rmove = new Move().randomMove()
+		//if (!equals(C.move(rmove),C)) {
+			route.addMove(rmove)
+			C = C.move(rmove)
+		//}
 		solved = equals(this.arr_pt,C)
 	} while (!solved);
 
@@ -67,19 +89,5 @@ console.log(A)
 console.log(B)
 
 var p = new Problem(A,B)
-console.log(p)
 p.solve()
 console.log(p)
-p.solve()
-console.log(p)
-
-console.log(randomMove())
-console.log(randomMove())
-console.log(randomMove())
-console.log(randomMove())
-console.log(randomMove())
-console.log(randomMove())
-console.log(randomMove())
-console.log(randomMove())
-console.log(randomMove())
-console.log(randomMove())
