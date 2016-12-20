25ac756255a29325670be29447b277820ccfca99
levg34 <guilhem.dross@laposte.net>
2016-12-20 18:54:03 +0100

    fix #4 display points

--- js/draw.js
+++ js/draw.js
@@ -1,6 +1,8 @@
+// get canvas and context
 var c1 = document.getElementById('canvas1')
 var ctx = c1.getContext('2d')
 
+// draw horizontal lines
 var baseY = c1.height/(BOARD.y+1)
 for (var i=0;i<BOARD.y;++i) {
     ctx.moveTo(0, baseY+i*baseY); 
@@ -8,9 +10,18 @@ for (var i=0;i<BOARD.y;++i) {
     ctx.stroke()
 }
 
+// draw vertical lines
 var baseX = c1.width/(BOARD.x+1)
 for (var i=0;i<BOARD.x;++i) {
     ctx.moveTo(baseX+i*baseX, 0); 
     ctx.lineTo(baseX+i*baseX, c1.height)
     ctx.stroke()
 }
+
+var pointSize = 40
+function drawPoint(p) {
+    ctx.fillRect(5+baseX*p.x,5+baseY*p.y,pointSize,pointSize)
+}
+
+drawPoint(A)
+drawPoint(B)
