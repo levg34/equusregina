4a57244e228752d0c3aa8f52434d360b43d509e9
levg34 <levg34@users.noreply.github.com>
2016-12-21 20:17:05 +0100

    4 space = tab in draw.js

--- js/draw.js
+++ js/draw.js
@@ -5,17 +5,17 @@ var ctx = c1.getContext('2d')
 // draw horizontal lines
 var baseY = c1.height/(BOARD.y+1)
 for (var i=0;i<BOARD.y;++i) {
-    ctx.moveTo(0, baseY+i*baseY)
-    ctx.lineTo(c1.width, baseY+i*baseY)
-    ctx.stroke()
+	ctx.moveTo(0, baseY+i*baseY)
+	ctx.lineTo(c1.width, baseY+i*baseY)
+	ctx.stroke()
 }
 
 // draw vertical lines
 var baseX = c1.width/(BOARD.x+1)
 for (var i=0;i<BOARD.x;++i) {
-    ctx.moveTo(baseX+i*baseX, 0)
-    ctx.lineTo(baseX+i*baseX, c1.height)
-    ctx.stroke()
+	ctx.moveTo(baseX+i*baseX, 0)
+	ctx.lineTo(baseX+i*baseX, c1.height)
+	ctx.stroke()
 }
 
 // draw point function
@@ -24,22 +24,22 @@ var borderPSY = baseY/10
 var pointSizeX = baseX-2*borderPSX
 var pointSizeY = baseY-2*borderPSY
 function drawPoint(p) {
-    ctx.fillRect(borderPSX+baseX*p.x,borderPSY+baseY*p.y,pointSizeX,pointSizeY)
+	ctx.fillRect(borderPSX+baseX*p.x,borderPSY+baseY*p.y,pointSizeX,pointSizeY)
 }
 
 // draw possible points to wich we can move from point p, 
 // those points will be drawn in color color
 function drawPossibleMovesColor(p,color) {
-    ctx.save()
-    ctx.fillStyle=color
-    var pm = p.possibleMoves()
-    for (var i in pm) {
-        drawPoint(p.move(pm[i]))
-    }
-    ctx.restore()
+	ctx.save()
+	ctx.fillStyle=color
+	var pm = p.possibleMoves()
+	for (var i in pm) {
+		drawPoint(p.move(pm[i]))
+	}
+	ctx.restore()
 }
 function drawPossibleMoves(p) {
-    drawPossibleMovesColor(p,"#FF0000")
+	drawPossibleMovesColor(p,"#FF0000")
 }
 function drawMove(p,move) {
 	//TODO
