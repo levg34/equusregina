4ae0a122641c61890e54e827666a192875a74bed
levg34 <guilhem.dross@laposte.net>
2016-12-23 16:00:02 +0100

    working on issue #7

--- js/script.js
+++ js/script.js
@@ -1,22 +1,59 @@
-// use them (main.js)
-var A = new Point(2, 5)
-var B = new Point(7, 3)
-console.log(A)
-console.log(B)
+var solveButton = document.getElementById('solve')
+var toolOption = document.getElementById('tool')
+var bestSolMoves = document.getElementById('nbMinMoves')
+var bs = document.getElementById('bs')
 
-var p = new Problem(A,B)
-p.solve()
-console.log(p)
+var A
+var B
+var p
 
-// use them (draw.js)
-drawPoint(A)
-drawPoint(B)
+var readyToSolve = false
 
-drawPossibleMoves(A)
-drawPossibleMovesColor(B,"green")
+function changeTool() {
+	var tool = toolOption.value
+	switch(tool) {
+		case 'A':
+			// TODO get click coordinates
+			A = new Point(2, 5)
+			drawPoint(A)
+			if (B) {
+				readyToSolve=true
+			}
+			break;
+		case 'B':
+			// TODO get click coordinates
+			B = new Point(7, 3)
+			drawPoint(B)
+			if (A) {
+				readyToSolve=true
+			}
+			break;
+		default:
+			// default
+	}
+	if (readyToSolve) {
+		toolOption.setAttribute('disabled','')
+		solveButton.removeAttribute('disabled')
+		p = new Problem(A,B)
+	} // not here
+}
 
-// other
-function addPoint(p) {
+function clickSolve() {
+	solveButton.setAttribute('disabled','')
+	p.solve()
+	if (solveButton.innerText == 'Solve') {
+		solveButton.innerText = 'Solve again'
+		bs.removeAttribute('hidden')
+	}
+	solveButton.removeAttribute('disabled')
+	
+	var minMoves = p.solutions[0].moves.length
+	for (var i in p.solutions) {
+		if (p.solutions[i].moves.length<minMoves) {
+			minMoves=p.solutions[i].moves.length
+		}
+	}
+	bestSolMoves.innerText = minMoves
+	
 	// TODO
-	drawPoint(p)
 }
