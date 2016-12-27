c535a6770118b57f744e5f1a03200ef1ac54e341
levg34 <guilhem.dross@laposte.net>
2016-12-27 16:01:26 +0100

    fix #12: code review

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // global methods
 
+// board dimensions, from 0 to BOARD.{x,y} (add 1 for board size)
 BOARD = {
 	x:7,
 	y:7
@@ -36,6 +37,7 @@ Move.randomMove = function() {
 	return Move.moves[m]
 }
 
+// return move number i, or i%8 if i>7
 Move.move = function(i) {
 	return Move.moves[i%8]
 }
@@ -45,10 +47,12 @@ function Route() {
 	this.moves=[]
 }
 
+// add move to route
 Route.prototype.addMove = function(m) {
 	this.moves.push(m)
 }
 
+// get move i in route
 Route.prototype.getMove = function(i) {
 	return this.moves[i]
 }
@@ -59,10 +63,12 @@ function Point(x,y) {
 	this.y=y
 }
 
+// point is in board
 Point.prototype.inBoard = function() {
 	return this.x >= 0 && this.y >= 0 && this.x <= BOARD.x && this.y <= BOARD.y
 }
 
+// return new point with coordinates after the move
 Point.prototype.move = function(m) {
 	var res = new Point()
 
@@ -76,6 +82,7 @@ Point.prototype.move = function(m) {
 	return res
 }
 
+// return a list of possible moves from that poins
 Point.prototype.possibleMoves = function() {
 	var pmoves = []
 	for (var i in Move.moves) {
@@ -87,6 +94,7 @@ Point.prototype.possibleMoves = function() {
 	return pmoves
 }
 
+// return new point with coordinates after all the moves from the route
 Point.prototype.route = function(r) {
 	var res = new Point(this.x,this.y)
 	
@@ -97,6 +105,7 @@ Point.prototype.route = function(r) {
 	return res
 }
 
+// returns the distance between this point and another point
 Point.prototype.distance = function(b) {
 	return Math.sqrt(Math.pow(b.x-this.x,2)+Math.pow(b.y-this.y,2))
 }
@@ -108,21 +117,10 @@ function Problem(start_pt,arr_pt) {
 	this.solutions=[] // Route[]
 }
 
+// solve the problem, fill in this.solutions
 Problem.prototype.solve = function() {
 	var route = new Route()
 	var C = this.start_pt
-	var visitedPoints = []
-	visitedPoints.push(C)
-	
-	function hasVisited(p) {
-		var res = false
-		for (var i in visitedPoints) {
-			if (equals(visitedPoints[i],p)) {
-				res = true
-			}
-		}
-		return res
-	}
 
 	while (!equals(this.arr_pt,C)) {
 		// get all the possible moves
@@ -132,8 +130,6 @@ Problem.prototype.solve = function() {
 		route.addMove(rmove)
 		// place C after the move (move C)
 		C = C.move(rmove)
-		// add C to the list of visited points
-		visitedPoints.push(C)
 	}
 
 	this.solutions.push(route)
